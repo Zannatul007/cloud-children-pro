51d505a0338d895dc7b8eba64561a2f3e7bc14c7
Zannatul Fardaush Tripty <65339499+Zannatul007@users.noreply.github.com>
2022-07-17 13:23:01 +0600

    add parenting guide

---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+let swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
